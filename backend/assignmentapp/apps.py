e29919980d2b6c45223f9eb067c93992522f3d15
JasperLKH <tomleehk2@gmail.com>
2025-08-22 12:04:42 +0800

    extracted the forum and assignment app

--- backend/assignmentapp/apps.py
+++ backend/assignmentapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReachappConfig(AppConfig):
+class AssignmentappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reachapp'
+    name = 'assignmentapp'
